160a3ad02c494096b4b1b0b0efc614d2600e6138
KKiyofuji <akusadss@gmail.com>
2021-07-09 18:10:22 +0900

    ブランチを切るテスト

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -8,4 +8,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        println("テスト") } }
+        println("テスト")
+        println("test")} }
